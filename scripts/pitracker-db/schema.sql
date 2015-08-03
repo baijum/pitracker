70395529896c6f2512c7c4804c02b09809f49772
Baiju Muthukadan <baiju.m.mail@gmail.com>
2015-08-03 09:01:31 +0530

    schema

---
+++ scripts/pitracker-db/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS "member" CASCADE;
+
+CREATE TABLE "member" (
+       id serial PRIMARY KEY,
+       username TEXT UNIQUE,
+       email TEXT,
+       password TEXT,
+       fullname TEXT
+);
+
+DROP TABLE IF EXISTS "project" CASCADE;
+
+CREATE TABLE "project" (
+       id serial PRIMARY KEY,
+       name TEXT,
+       description TEXT,
+       archived BOOLEAN DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS "item" CASCADE;
+
+CREATE TABLE "item" (
+       id SERIAL PRIMARY KEY,
+       title TEXT,
+       description TEXT,
+       project_id BIGINT REFERENCES project(id)
+);
+
+DROP TABLE IF EXISTS "comment" CASCADE;
+
+CREATE TABLE "comment" (
+       id BIGSERIAL PRIMARY KEY,
+       content TEXT,
+       item_id BIGINT REFERENCES item(id)
+);
