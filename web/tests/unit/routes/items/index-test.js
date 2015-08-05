a7e31ca96e0c84d1ae446d602bfe81d60a1ed056
Baiju Muthukadan <baiju.m.mail@gmail.com>
2015-08-05 23:13:41 +0530

    list items

---
+++ web/tests/unit/routes/items/index-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:items/index', 'Unit | Route | items/index', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
